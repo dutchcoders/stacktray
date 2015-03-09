65058859b2dc7bbe04e5f32a58416b8c5c6cf899
rubencagnie <ruben.cagnie@gmail.com>
2015-03-09 13:51:32 -0400

    Initial Commit

---
+++ StackTray/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StackTray
+//
+//  Created by Ruben Cagnie on 3/9/15.
+//  Copyright (c) 2015 dutchcoders. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
