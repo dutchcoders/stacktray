042057782ce28145709179d280cb291c18b9cc18
rubencagnie <ruben.cagnie@gmail.com>
2015-06-06 16:08:25 -0400

    Launch at startup

--- StackTray/SettingsViewController.swift
+++ StackTray/SettingsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import ServiceManagement
 
 class SettingsViewController: NSViewController {
   var settingsDetail : SettingsDetailViewController!
@@ -71,4 +72,82 @@ class SettingsDetailViewController : NSViewController {
     didSet{
     }
   }
+    
+    @IBOutlet weak var launchAtStartup: NSButton! {
+        didSet{
+            launchAtStartup.state = applicationIsInStartUpItems() ? NSOnState : NSOffState
+        }
+    }
+    
+    @IBAction func toggleStartup(sender: AnyObject) {
+        toggleLaunchAtStartup()
+    }
+    
+    
+    func applicationIsInStartUpItems() -> Bool {
+        return (itemReferencesInLoginItems().existingReference != nil)
+    }
+    
+    func itemReferencesInLoginItems() -> (existingReference: LSSharedFileListItemRef?, lastReference: LSSharedFileListItemRef?) {
+        var itemUrl : UnsafeMutablePointer<Unmanaged<CFURL>?> = UnsafeMutablePointer<Unmanaged<CFURL>?>.alloc(1)
+        if let appUrl : NSURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath) {
+            let loginItemsRef = LSSharedFileListCreate(
+                nil,
+                kLSSharedFileListSessionLoginItems.takeRetainedValue(),
+                nil
+                ).takeRetainedValue() as LSSharedFileListRef?
+            if loginItemsRef != nil {
+                let loginItems: NSArray = LSSharedFileListCopySnapshot(loginItemsRef, nil).takeRetainedValue() as NSArray
+                println("There are \(loginItems.count) login items")
+                let lastItemRef: LSSharedFileListItemRef = loginItems.lastObject as! LSSharedFileListItemRef
+                for var i = 0; i < loginItems.count; ++i {
+                    let currentItemRef: LSSharedFileListItemRef = loginItems.objectAtIndex(i) as! LSSharedFileListItemRef
+                    if LSSharedFileListItemResolve(currentItemRef, 0, itemUrl, nil) == noErr {
+                        if let urlRef: NSURL =  itemUrl.memory?.takeRetainedValue() {
+                            println("URL Ref: \(urlRef.lastPathComponent)")
+                            if urlRef.isEqual(appUrl) {
+                                return (currentItemRef, lastItemRef)
+                            }
+                        }
+                    } else {
+                        println("Unknown login application")
+                    }
+                }
+                //The application was not found in the startup list
+                return (nil, lastItemRef)
+            }
+        }
+        return (nil, nil)
+    }
+    
+    func toggleLaunchAtStartup() {
+        let itemReferences = itemReferencesInLoginItems()
+        let shouldBeToggled = (itemReferences.existingReference == nil)
+        let loginItemsRef = LSSharedFileListCreate(
+            nil,
+            kLSSharedFileListSessionLoginItems.takeRetainedValue(),
+            nil
+            ).takeRetainedValue() as LSSharedFileListRef?
+        if loginItemsRef != nil {
+            if shouldBeToggled {
+                if let appUrl : CFURLRef = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath) {
+                    LSSharedFileListInsertItemURL(
+                        loginItemsRef,
+                        itemReferences.lastReference,
+                        nil,
+                        nil,
+                        appUrl,
+                        nil,
+                        nil
+                    )
+                    println("Application was added to login items")
+                }
+            } else {
+                if let itemRef = itemReferences.existingReference {
+                    LSSharedFileListItemRemove(loginItemsRef,itemRef);
+                    println("Application was removed from login items")
+                }
+            }
+        }
+    }
 }
